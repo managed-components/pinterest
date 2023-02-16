39e4ff59550acdf339e9e88debe23721b64942b0
Omar Mohammad <omarmo@cloudflare.com>
2023-02-16 13:23:15 +0000

    Initial commit

---
+++ src/index.ts
@@ -0,0 +1,116 @@
+import { ComponentSettings, Manager, MCEvent } from '@managed-components/types'
+
+export type AutomaticDataType = {
+  loc: string
+  ref: string
+  if: boolean
+  noscript?: number
+  sh?: string
+  sw?: string
+  mh?: string
+}
+
+export type RequestBodyType = {
+  ad: string
+  cb: string
+  tid: string
+  event: string
+  'pd[em]'?: string
+  'pd[tm]'?: string
+  ed?: string
+}
+
+export const getRequestBody = (
+  eventType: string,
+  event: MCEvent,
+  settings: ComponentSettings
+) => {
+  const { client, payload } = event
+
+  const automaticData: AutomaticDataType = {
+    loc: client.url.href,
+    ref: client.referer,
+    if: false, // TODO: add an iframe check?
+    // noscript=1 // not sure if we should announce this?
+    sh: client.screenHeight?.toString(),
+    sw: client.screenWidth?.toString(),
+    mh: '2424edb5', // this seems to be a hardocded Pinterest version, perhaps a git commit?
+  }
+
+  const requestBody: RequestBodyType = {
+    ad: JSON.stringify(automaticData),
+    cb: new Date().valueOf().toString(),
+    tid: payload.tid || settings.tid,
+    event: eventType,
+  }
+
+  const { 'pd[em]': pdem, tid, ...cleanPayload } = payload
+
+  // pd - partner data
+  if (pdem) {
+    requestBody['pd[em]'] = pdem
+  }
+
+  requestBody['pd[tm]'] = payload.tm || 'pinterest-mc'
+
+  if (Object.keys(cleanPayload).length) {
+    // event data
+    requestBody['ed'] = JSON.stringify(cleanPayload)
+  }
+
+  return requestBody
+}
+
+export const getRequestUrl = (
+  requestBody: RequestBodyType,
+  event: MCEvent,
+  settings: ComponentSettings
+): string => {
+  const queryParams = new URLSearchParams(requestBody).toString()
+  return `https://ct.pinterest.com/v3/?${queryParams}`
+}
+
+export const sendRequest = (url: string, event: MCEvent) => {
+  event.client.fetch(url, {
+    mode: 'no-cors',
+    keepalive: true,
+    credentials: 'include',
+  })
+}
+
+export const handler = (
+  eventType: string,
+  event: MCEvent,
+  settings: ComponentSettings,
+  customSendRequest = sendRequest
+) => {
+  const requestBody = getRequestBody(eventType, event, settings)
+  const requestUrl = getRequestUrl(requestBody, event, settings)
+  customSendRequest(requestUrl, event)
+}
+
+export default async function (manager: Manager, settings: ComponentSettings) {
+  manager.addEventListener('pageview', event => {
+    handler('pagevisit', event, settings)
+  })
+
+  manager.addEventListener('addtocart', event => {
+    handler('addtocart', event, settings)
+  })
+
+  manager.addEventListener('checkout', event => {
+    handler('checkout', event, settings)
+  })
+
+  manager.addEventListener('lead', event => {
+    handler('lead', event, settings)
+  })
+
+  manager.addEventListener('signup', event => {
+    handler('signup', event, settings)
+  })
+
+  manager.addEventListener('watchVideo', event => {
+    handler('watchVideo', event, settings)
+  })
+}
